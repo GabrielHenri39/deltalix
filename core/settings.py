0d9672099e5bd52bae54188d529362b0ca2b6e7a
Gabriel <gabriel688dias@hotmail.com>
2025-10-14 22:02:47 -0300

    corecções

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 if DEBUG:
     SITE_URL = 'http://localhost:8000'
 else:
-    SITE_URL = 'https://deltalixabiental.com.br'
+    SITE_URL = 'https://deltalixambiental.com.br'
 
 
 # Application definition
@@ -180,6 +180,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 INTERNAL_IPS = [
     '127.0.0.1',
